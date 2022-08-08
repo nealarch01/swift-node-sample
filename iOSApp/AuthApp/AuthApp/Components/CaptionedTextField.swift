4804c18722cc6c751eda8f4e255e0d8b341bf6d3
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-08-08 12:54:57 -0700

    Fix component relative positions

--- iOSApp/AuthApp/AuthApp/Components/CaptionedTextField.swift
+++ iOSApp/AuthApp/AuthApp/Components/CaptionedTextField.swift
@@ -12,29 +12,31 @@ struct CaptionedTextField: View {
     @Binding var text: String
     let placeholder: String
     var body: some View {
-        VStack {
-            HStack {
-                Text(caption)
-                    .font(.system(size: 22, weight: .medium))
-                    .padding([.leading], 40)
-                    .padding([.bottom], -2)
-                    .foregroundColor(Color.black)
-                Spacer()
+        GeometryReader { geometry in
+            VStack {
+                HStack {
+                    Text(caption)
+                        .font(.system(size: 22, weight: .medium))
+                        .foregroundColor(Color.black)
+                    Spacer()
+                }
+                .frame(width: 310)
+                
+                TextField(placeholder, text: $text)
+                    .padding()
+                    .frame(width: 310, height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(12)
+                    .disableAutocorrection(true)
             }
-            
-            TextField(placeholder, text: $text)
-                .padding()
-                .frame(width: 310, height: 50)
-                .background(Color.black.opacity(0.05))
-                .cornerRadius(12)
-                .disableAutocorrection(true)
-        }
-        .padding([.top, .bottom], 5)
+            .frame(width: geometry.size.width) // Align center
+        } // End of Geometry Reader
+        .frame(height: 100)
     }
 }
 
 struct CaptionedTextField_Previews: PreviewProvider {
     static var previews: some View {
-        CaptionedTextField(caption: "", text: .constant(""), placeholder: "")
+        CaptionedTextField(caption: "Username", text: .constant(""), placeholder: "Enter username")
     }
 }
--- iOSApp/AuthApp/AuthApp/Components/ViewableSecureField.swift
+++ iOSApp/AuthApp/AuthApp/Components/ViewableSecureField.swift
@@ -17,44 +17,47 @@ struct ViewableSecureField: View {
     @State private var isSecure: Bool = true
     
     var body: some View {
-        VStack {
-            HStack {
-                Text(caption)
-                    .font(.system(size: 22, weight: .medium))
-                    .padding([.leading], 40)
-                    .padding([.bottom], -2)
-                    .foregroundColor(Color.black)
-                Spacer()
-            }
-            HStack {
-                if isSecure == true {
-                    SecureField(placeholder, text: $text)
-                        .padding()
-                        .frame(width: 310, height: 50)
-                        .background(Color.black.opacity(0.05))
-                        .cornerRadius(12)
-                        .disableAutocorrection(true)
-                } else {
-                    TextField(text == "" ? "Enter password" : text, text: $text)
-                        .padding()
-                        .frame(width: 310, height: 50)
-                        .background(Color.black.opacity(0.05))
-                        .cornerRadius(12)
-                        .disableAutocorrection(true)
+        GeometryReader { geometry in
+            VStack {
+                HStack {
+                    Text(caption)
+                        .font(.system(size: 22, weight: .medium))
+                        .foregroundColor(Color.black)
+                    Spacer()
                 }
-            }.overlay(alignment: .trailing) {
-                Image(systemName: isSecure == true ? "eye.slash" : "eye")
-                    .onTapGesture {
-                        isSecure.toggle()
-                    }.padding([.trailing], 10)
+                .frame(width: 310)
+                
+                HStack {
+                    if isSecure == true {
+                        SecureField(placeholder, text: $text)
+                            .padding()
+                            .frame(width: 310, height: 50)
+                            .background(Color.black.opacity(0.05))
+                            .cornerRadius(12)
+                            .disableAutocorrection(true)
+                    } else {
+                        TextField(text == "" ? "Enter password" : text, text: $text)
+                            .padding()
+                            .frame(width: 310, height: 50)
+                            .background(Color.black.opacity(0.05))
+                            .cornerRadius(12)
+                            .disableAutocorrection(true)
+                    }
+                }.overlay(alignment: .trailing) {
+                    Image(systemName: isSecure == true ? "eye.slash" : "eye")
+                        .onTapGesture {
+                            isSecure.toggle()
+                        }.padding([.trailing], 10)
+                }
             }
-        }
-        .padding([.top, .bottom], 5)
+            .frame(width: geometry.size.width)
+        } // End of GeometryReader
+        .frame(height: 100)
     }
 }
 
 struct ViewableSecureField_Previews: PreviewProvider {
     static var previews: some View {
-        ViewableSecureField(caption: "Password", text: .constant(""), placeholder: "")
+        ViewableSecureField(caption: "Password", text: .constant(""), placeholder: "Enter password")
     }
 }
